02aa5d4da300be89ff337c3a714b99d53a61df39
aagavin <aagavin@gmail.com>
2018-09-15 21:19:29 -0400

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,47 @@
-import { Page, Browser, Cookie, launch } from "puppeteer";
-import { config as AWSConfig, DynamoDB, AWSError, Request } from "aws-sdk";
+import { config as AWSConfig, DynamoDB, AWSError } from "aws-sdk";
+import { PromiseResult } from "aws-sdk/lib/request";
 
 import to from "./util/to";
+import setCookies from "./setCookies";
 
-const URL: string = "https://www.prestocard.ca/en/";
-const SIGN_IN_LINK_SELECTOR: string = "body > header > div.header.container > div.main-navigation > ul.nav.navbar-nav.navbar-right > li.modalLogin > a";
 const TABLE_NAME = "prestoCache";
 
 AWSConfig.update({ region: 'us-east-1' });
 const cacheDBDocClient = new DynamoDB.DocumentClient();
 
-
-exports.handler = async (event): Promise<{}> => {
-
-    if (typeof event.command !== 'undefined' && event.command === 'AddCreds') {
-        return addCreds(event);
+const setCookiesLoop = async (): Promise<void> => {
+    const params: DynamoDB.DocumentClient.ScanInput = {
+        TableName: TABLE_NAME,
+        AttributesToGet: ['username', 'cred']
     }
-    return getCookies(event);
-}
+    const userResult: PromiseResult<DynamoDB.DocumentClient.ScanOutput, AWSError> = await cacheDBDocClient.scan(params).promise();
 
-this.handler({ username: 'lorraineif', password: '56wYM4Bies', command: 'AddCreds' }).then(console.log).catch(console.log);
+    if (userResult.$response.error) {
+        console.error(userResult.$response.error.message);
+    }
+
+    const users: Array<{ username: string, cred: string }> = userResult.$response.data['Items'];
+
+    users.map(user => user.cred = Buffer.from(user.cred, 'base64').toString('ascii'));
+    users.forEach(async user => {
+        const cookies = await setCookies(user);
+        const parmas: DynamoDB.DocumentClient.UpdateItemInput = {
+            TableName: TABLE_NAME,
+            Key: {
+                'username': user.username
+            },
+            UpdateExpression: 'SET cookiesCache = :c',
+            ExpressionAttributeValues: {
+                ':c': Buffer.from(JSON.stringify(cookies)).toString('base64')
+            },
+            ReturnValues: "UPDATED_NEW",
+        };
+
+        cacheDBDocClient.update(parmas, (err: AWSError, data) => {
+            if (err) console.error(err.message);
+        });
+    });
+}
 
 const addCreds = async (event: any) => {
     const params: DynamoDB.DocumentClient.PutItemInput = {
@@ -29,81 +51,20 @@ const addCreds = async (event: any) => {
             cred: Buffer.from(event.password).toString('base64')
         }
     };
-    const dbValue: Request<DynamoDB.DocumentClient.PutItemOutput, AWSError> = cacheDBDocClient.put(params, (err: AWSError, data: DynamoDB.DocumentClient.PutItemOutput) => {
-        if (err)
-            console.error(err.message);
-    });
+    const dbValue: PromiseResult<DynamoDB.DocumentClient.PutItemOutput, AWSError> = await cacheDBDocClient.put(params).promise();
+    if (dbValue.$response.error) console.log(dbValue.$response.error);
     return {};
 }
 
-const getCookies = async (event) => {
+exports.handler = async (event): Promise<object> => {
+    let err;
 
-    let browser: Browser, page: Page, cookies: Array<Cookie>, err;
-
-    console.log('opening browser');
-    [err, browser] = await to(launch({
-        // executablePath: './headless-chromium',
-        headless: false,
-        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--single-process', '--deterministic-fetch', "--proxy-server='direct://'", '--proxy-bypass-list=*']
-    }));
-    if (err) console.error(err);
-
-    console.log('opening page');
-    [err, page] = await to(browser.newPage());
-    if (err) console.error(err);
-
-    await page.setViewport({ 'width': 1920, 'height': 1080 });
-
-    console.log(`going to ${URL}`);
-    await Promise.all([
-        page.waitForNavigation(),
-        await page.goto(URL)
-    ]);
-
-    await page.click(SIGN_IN_LINK_SELECTOR);
-    await page.waitFor(300);
-    await page.click('#SignIn_Username');
-
-    await Promise.all([
-        await page.waitFor(50),
-        await page.type('#SignIn_Username', event.username)
-    ]);
-
-    await page.click('#SignIn_Password');
-
-    await Promise.all([
-        await page.waitFor(50),
-        page.type('#SignIn_Password', event.password)
-    ]);
-
-    await Promise.all([
-        page.waitForNavigation(),
-        page.click('#btnsubmit')
-    ]);
-
-    [err, cookies] = await to(page.cookies());
-    if (err) console.error(err);
-
-    console.log('closeing page and browser')
-    await page.close();
-    await browser.close();
-
-    const parmas: DynamoDB.DocumentClient.UpdateItemInput = {
-        TableName: TABLE_NAME,
-        Key: {
-            'username': event.username
-        },
-        UpdateExpression: 'SET cookiesCache = :c',
-        ExpressionAttributeValues: {
-            ':c': Buffer.from(JSON.stringify(cookies)).toString('base64')
-        },
-        ReturnValues: "UPDATED_NEW",
-    };
-
-    cacheDBDocClient.update(parmas, (err: AWSError, data) => {
-        if (err) console.error(err.message);
-    });
-
-    console.log('returning cookies')
-    return {};
+    if (typeof event.command !== 'undefined' && event.command === 'AddCreds') return addCreds(event);
+    [err,] = await to(setCookiesLoop());
+    if (err) {
+        console.error(err);
+        return err;
+    }
+    return { success: true };
 }
+
