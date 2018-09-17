a381634b2601bddf73ed088fc52edf1f7d0e91e3
aagavin <aagavin@gmail.com>
2018-09-16 20:09:20 -0400

    grrrrrr mixing async stuff

--- src/index.ts
+++ src/index.ts
@@ -12,60 +12,62 @@ const TABLE_NAME = "prestoCache";
 AWSConfig.update({ region: 'us-east-1' });
 const cacheDBDocClient = new DynamoDB.DocumentClient();
 
-const setCookiesLoop = async (callback): Promise<void> => {
-    console.log('setting cookies');
-    const params: DynamoDB.DocumentClient.ScanInput = {
-        TableName: TABLE_NAME,
-        AttributesToGet: ['username', 'cred']
-    }
-    const userResult: PromiseResult<DynamoDB.DocumentClient.ScanOutput, AWSError> = await cacheDBDocClient.scan(params).promise();
+const setCookiesLoop = async (): Promise<void> => {
 
-    if (userResult.$response.error) {
-        console.error(userResult.$response.error.message);
-    }
-
-    let users: Array<object> = userResult.$response.data['Items'];
-
-    users = users.map(user => {
-        user['password'] = Buffer.from(user['cred'], 'base64').toString('ascii')
-        return user;
-    });
-
-    users.forEach(async user => {
-
-        const params: Lambda.InvocationRequest = {
-            FunctionName: 'presto-cookies',
-            Payload: JSON.stringify(user)
-        };
-
-        console.log('-----------1------------');
-        (new Lambda()).invoke(params, (err, cookies) => {
-
-            console.log(`-------------${cookies}--------`)
-            const parmas: DynamoDB.DocumentClient.UpdateItemInput = {
-                TableName: TABLE_NAME,
-                Key: {
-                    'username': user['username']
-                },
-                UpdateExpression: 'SET cookiesCache = :c, lastUpdate = :d',
-                ExpressionAttributeValues: {
-                    ':c': Buffer.from(JSON.stringify(cookies)).toString('base64'),
-                    ':d': new Date().valueOf()
-                },
-                ReturnValues: "UPDATED_NEW",
-            };
-            console.log(JSON.stringify(params));
-            cacheDBDocClient.update(parmas, (err: AWSError, data) => {
-                if (err) console.error(err.message);
-                console.log(data);
-            });
-            console.log('-----------1.5------------')
-            callback(true);
+    return new Promise<void>(async (resolve, reject) => {
+        console.log('setting cookies');
+        const params: DynamoDB.DocumentClient.ScanInput = {
+            TableName: TABLE_NAME,
+            AttributesToGet: ['username', 'cred']
+        }
+        const userResult: PromiseResult<DynamoDB.DocumentClient.ScanOutput, AWSError> = await cacheDBDocClient.scan(params).promise();
+    
+        if (userResult.$response.error) {
+            console.error(userResult.$response.error.message);
+        }
+    
+        let users: Array<object> = userResult.$response.data['Items'];
+    
+        users = users.map(user => {
+            user['password'] = Buffer.from(user['cred'], 'base64').toString('ascii')
+            return user;
         });
-        console.log('-----------2------------');
-
+    
+        users.forEach(async user => {
+    
+            const params: Lambda.InvocationRequest = {
+                FunctionName: 'presto-cookies',
+                Payload: JSON.stringify(user)
+            };
+    
+            console.log('-----------1------------');
+            (new Lambda()).invoke(params, (err, cookies) => {
+    
+                console.log(`-------------${cookies}--------`)
+                const parmas: DynamoDB.DocumentClient.UpdateItemInput = {
+                    TableName: TABLE_NAME,
+                    Key: {
+                        'username': user['username']
+                    },
+                    UpdateExpression: 'SET cookiesCache = :c, lastUpdate = :d',
+                    ExpressionAttributeValues: {
+                        ':c': Buffer.from(JSON.stringify(cookies)).toString('base64'),
+                        ':d': new Date().valueOf()
+                    },
+                    ReturnValues: "UPDATED_NEW",
+                };
+                console.log(JSON.stringify(params));
+                cacheDBDocClient.update(parmas, (err: AWSError, data) => {
+                    if (err) console.error(err.message);
+                    resolve();
+                });
+                console.log('-----------1.5------------')
+            });
+            console.log('-----------2------------');
+    
+        });
     });
-}
+} //setCookiesLoop
 
 const addCreds = async (event: any) => {
     const params: DynamoDB.DocumentClient.PutItemInput = {
@@ -87,7 +89,7 @@ exports.handler = async (event): Promise<object> => {
         console.log('added creds');
         return addCreds(event);
     }
-    [err] = await to(setCookiesLoop((val: boolean) => val));
+    [err] = await to(setCookiesLoop());
     if (err) {
         console.error(err);
         return err;
