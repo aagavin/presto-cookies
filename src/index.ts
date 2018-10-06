088095884af79ea39b293f3316915c0d44188f39
aagavin <aagavin@gmail.com>
2018-10-06 17:31:13 -0400

    error handling

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,19 @@ import { logError } from "./util/error";
 const URL: string = "https://www.prestocard.ca/en/";
 const SIGN_IN_LINK_SELECTOR: string = "body > header > div.header.container > div.main-navigation > ul.nav.navbar-nav.navbar-right > li.modalLogin > a";
 
+const parseSignInError = async (page: Page, browser: Browser) => {
+    const errorResponse = await page.evaluate(() => {
+        const usernameError = document.getElementById('error_SignIn_Username');
+        const passwordError = document.getElementById('error_SignIn_Password');
+        return {
+            usernameError: usernameError ? usernameError.innerText : usernameError,
+            passwordError: passwordError ? passwordError.innerText : passwordError
+        };
+    });
+    await browser.close();
+    return errorResponse;
+}
+
 exports.handler = async (event): Promise<{}> => {
 
     let browser: Browser, page: Page, err;
@@ -16,17 +29,17 @@ exports.handler = async (event): Promise<{}> => {
         executablePath: './headless-chromium',
         args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--single-process', '--deterministic-fetch', "--proxy-server='direct://'", '--proxy-bypass-list=*']
     }));
-    if (err) console.error(await page.content, err);
+    if (err) console.error('error with browser', err);
 
     console.log('opening page');
     [err, page] = await to(browser.newPage());
-    if (err) console.error(await page.content, err);
+    if (err) console.error('page error', err);
 
     await page.setViewport({ 'width': 1920, 'height': 1080 });
 
     console.log(`going to ${URL}`);
     await Promise.all([
-        page.waitForNavigation({waitUntil: 'networkidle0'}),
+        page.waitForNavigation({ waitUntil: 'networkidle0' }),
         page.goto(URL)
     ]);
 
@@ -44,10 +57,12 @@ exports.handler = async (event): Promise<{}> => {
 
     console.log('click submit button');
     await page.waitFor(50);
-    await Promise.all([
-        page.waitForNavigation(),
+    [err,] = await to(Promise.all([
+        page.waitForNavigation({ timeout: 15 }),
         page.click('#btnsubmit')
-    ]);
+    ]));
+
+    if (err) return await parseSignInError(page, browser);
 
     const parsedData = await Promise.all([
         getBalance(page),
@@ -65,3 +80,4 @@ exports.handler = async (event): Promise<{}> => {
         otherCard: parsedData[2]
     };
 }
+
