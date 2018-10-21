9acee5e558e01a21d68bf6757ffe28a98513e9a9
aagavin <aagavin@gmail.com>
2018-10-20 20:38:35 -0400

    cache and chache check

---
+++ src/util/cache.ts
@@ -0,0 +1,81 @@
+import AWS = require('aws-sdk');
+import { PutItemInput, GetItemInput, GetItemOutput } from 'aws-sdk/clients/dynamodb';
+import { to } from './to';
+
+AWS.config.apiVersions = {
+    dynamodb: '2012-08-10',
+};
+AWS.config.update({ region: 'us-east-1' });
+
+const TABLENAME: string = 'PrestoCache';
+const dynamodb = new AWS.DynamoDB();
+
+/**
+ *
+ *
+ * @param {string} username
+ * @param {object} cardData
+ * @returns {Promise<object>}
+ */
+export const addToCache = async (username: string, cardData: object): Promise<object> => {
+    // 3600000
+    let result, err;
+
+    const encodedCardData = Buffer.from(JSON.stringify(cardData)).toString('base64');
+    const parmas: PutItemInput = {
+        Item: {
+            'username': {
+                S: username
+            },
+            'cardData': {
+                S: encodedCardData
+            },
+            'expireTime': {
+                S: (Math.floor(Date.now() / 1000) + 30).toString()
+            }
+        },
+        TableName: TABLENAME
+    };
+    [err, result] = await to(dynamodb.putItem(parmas).promise());
+    if (err) console.error(err);
+
+    return result;
+};
+
+/**
+ * get cached 
+ *
+ * @param {string} username
+ * @returns {Promise<object>}
+ */
+const getCache = async (username: string): Promise<object> => {
+
+    const params: GetItemInput = {
+        Key: {
+            'username': {
+                S: username
+            }
+        },
+        TableName: TABLENAME
+    }
+    const cacheValue: GetItemOutput = await dynamodb.getItem(params).promise();
+
+    const nowDate = Math.floor(Date.now() / 1000);
+    const expTime = parseInt(cacheValue.Item.expireTime.S);
+
+    if (nowDate > expTime) {
+        return null;
+    }
+
+    const jsonValue = Buffer.from(cacheValue.Item.cardData.S, 'base64').toString();
+    return JSON.parse(jsonValue);
+
+}
+
+export const checkCache = async (username: string): Promise<object> => {
+    const cacheValue = await getCache(username);
+    if (cacheValue !== null) {
+        console.info('using cache value');
+        return cacheValue;
+    }
+}
